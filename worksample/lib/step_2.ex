a8d9b5064a318a55a537b9ffb3dd98b2c4b5aa75
Jean Pignouf <maxime.de-la-fouchardiere@epitech.eu>
2017-03-20 12:50:30 +0100

    Ajout des fonctions de list

---
+++ worksample/lib/step_2.ex
@@ -0,0 +1,21 @@
+defmodule ElixirWorksample.Step2 do
+    def i_am([]) do
+        "empty list"    
+    end 
+
+    def i_am(data) when is_list(data) do
+        "list"
+    end
+
+    def i_am(data) when is_integer(data) do
+        "int"
+    end
+
+    def i_am(data) when is_bitstring(data) do
+        "string"
+    end
+
+    def i_am() do
+        "Nothing"
+    end
+end
